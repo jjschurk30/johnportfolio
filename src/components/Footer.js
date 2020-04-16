4c886fcc6ef4f3c9c80590b1090ff6ea8270d7ef
John Schurk <johnschurk@yahoo.com>
2020-04-16 14:01:22 -0500

    syntax error

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,7 @@ function Footer() {
                 <Row className="border-top justify-content-between p-3">
                     <Col className="p-0" md={3} sm={12}>
                        John Schurk
-                    </Col>;
+                    </Col>
                 </Row>
             </Container>
         </footer>
